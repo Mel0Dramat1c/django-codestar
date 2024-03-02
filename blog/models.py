2082f630d92255a5503251c23c2faebedcbecc3d
Mel Maguire <reishyn616@gmail.com>
2024-03-02 22:38:00 +0000

    add templates/blod and update views

--- blog/models.py
+++ blog/models.py
@@ -30,4 +30,8 @@ class Comment(models.Model):
     body = models.TextField()
     approved = models.BooleanField(default=False)
     created_on = models.DateTimeField(auto_now_add=True)
+    class Meta:
+        ordering = ["-created_on"]
+    def __str__(self):
+        return f"Comment {self.body} | written by {self.author}"
 
