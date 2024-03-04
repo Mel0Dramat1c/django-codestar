fd66592a963ef79ffd379ddfe5e49b4ee1987ee4
Mel Maguire <reishyn616@gmail.com>
2024-03-04 23:29:27 +0000

    update form

--- about/views.py
+++ about/views.py
@@ -7,6 +7,13 @@ def about_me(request):
     """
     Renders the About page
     """
+
+    if request.method == "POST":
+        collaborate_form = CollaborateForm(data=request.POST)
+        if collaborate_form.is_valid():
+            collaborate_form.save()
+            messages.add_message(request, messages.SUCCESS, "Collaboration request received! I endeavour to respond within 2 working days.")
+
     about = About.objects.all().order_by('-updated_on').first()
     collaborate_form = CollaborateForm()
 
